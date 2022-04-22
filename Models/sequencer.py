d6632babe63d9ae448fd79da83bac373d352baa0
Westen0511 <xiyuanjunzhi@gmail.com>
2022-04-23 00:05:03 +0900

    Add ImageSequencer

--- Models/sequencer.py
+++ Models/sequencer.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-from typing import Optional, TypedDict
-import re
+from typing import TypedDict
 
 import config
 
@@ -10,10 +9,39 @@ class LabeledImage(TypedDict):
 
 
 class ImageSequencer():
-    def __init__(self, images: list[Path]) -> None:
-        self.images: list[Path] = images
+    """順序付ける。"""
+    def __init__(self, contents: list[Path]) -> None:
+        self.image_paths: list[Path] = contents
+
+    def get_image_paths_as_str(self):
+        return [str(i) for i in self.image_paths]
+
+    def sort_based_on_numeric_part(self, numeric_part_index=-1):
+        try:
+            # stemに変換 → 指定されたインデックスを基準にsplit → 数値順にソート
+            self.image_paths = sorted(self.image_paths, key=lambda s: int(self.__get_partial(s.stem, numeric_part_index)))
+        # 数値以外が基準に指定された場合弾く
+        # TODO GUI コール
+        except ValueError:
+            raise ValueError("Non-numeric value is specified")
+
+    def sort_default(self):
+        self.image_paths = sorted(self.image_paths, key=lambda i: i.stem)
+
+    def sort_based_on_createtime(self):
+        self.image_paths = sorted(self.image_paths, key=lambda i: i.stat().st_ctime)
+
+    def labeling_images(self, label_part_index=-1):
         # {group_id: [LabeldImage...]...}
-        self.labeld_images: dict[str, list[LabeledImage]] = {}
+        labeld_images: dict[str, list[LabeledImage]] = {}
+
+        for image in self.image_paths:
+            label, group_id = self.__split_into_label_and_others(image.stem, label_part_index)
+            if group_id not in labeld_images.keys():
+                labeld_images[group_id] = []
+            labeld_images[group_id].append(LabeledImage(label=label, path=str(image)))
+
+        return labeld_images
 
     def __get_partial(self, string: str, part_index=-1):
         try:
@@ -27,32 +55,8 @@ class ImageSequencer():
         try:
             label = splits[label_part_index]
         except IndexError:
-            raise IndexError(f"Partial splits fail. string: {string}, index: {label_part_index}")
+            raise IndexError(f"Label/others splits fail. string: {string}, index: {label_part_index}")
         without_label = [i for i in splits if not i == label]
         without_label = config.DELIMITER.join(without_label)
 
         return label, without_label
-
-    def sort_based_on_numeric_part(self, numeric_part_index=-1):
-        try:
-            # Pathオブジェクトをstem(ファイル名のみ)に変換
-            # 指定されたインデックスを基準にsplit
-            # 数値順にソート
-            self.images = sorted(self.images, key=lambda s: int(self.__get_partial(s.stem, numeric_part_index)))
-        # 数値以外が基準に指定された場合弾く
-        # TODO GUI コール
-        except ValueError:
-            raise ValueError("Non-numeric value is specified")
-
-    def sort_default(self):
-        self.images = sorted(self.images)
-
-    def sort_based_on_createtime(self):
-        self.images = sorted(self.images, key=lambda i: i.stat().st_ctime)
-
-    def labeling_images(self, label_part_index=-1):
-        for image in self.images:
-            label, group_id = self.__split_into_label_and_others(image.stem, label_part_index)
-            if group_id not in self.labeld_images.keys():
-                self.labeld_images[group_id] = []
-            self.labeld_images[group_id].append(LabeledImage(label=label, path=str(image)))
