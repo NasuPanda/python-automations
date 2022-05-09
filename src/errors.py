c54a8eaa12e7c5c3cc4652467c6e53eee05270bb
NasuPanda <xiyuanjunzhi@gmail.com>
2022-05-09 11:37:18 +0900

    Add errors.py

--- src/errors.py
+++ src/errors.py
@@ -16,12 +16,3 @@ class NumberOfLabelMissMatchError(Exception):
 
 class SortBasePartNotNumberError(Exception):
     """ソートの基準となる部分に数字以外が指定されたとき"""
-
-
-try:
-    raise GroupIdNotNumberError("グループIDが数字ではありません。")
-except Exception as e:
-    print(e)
-    print(type(str(e)))
-
-# NOTE: 引数として渡された文字列を発生したエラーから取り出すことが出来る(Exception as eで)
