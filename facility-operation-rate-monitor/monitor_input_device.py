db1cc4659c26a84b438a2c41dc94f8146b139fb3
NasuPanda <xiyuanjunzhi@gmail.com>
2022-06-28 09:50:29 +0900

    feat: Enable monitoring input device in specific time range

--- facility-operation-rate-monitor/monitor_input_device.py
+++ facility-operation-rate-monitor/monitor_input_device.py
@@ -3,6 +3,8 @@ from pynput import keyboard
 from pynput.mouse import Listener as MouseListener
 from pynput.keyboard import Listener as KeyboardListener
 
+from timehelper import TimeHelper
+
 
 def on_press(key):
     print("Key pressed: {0}".format(key))
@@ -35,30 +37,39 @@ def monitoring_input_device():
     mouse_listener.start()
 
     # NOTE: join
-    # スレッドが終了するまで待機する。joinを呼ばれたスレッドが終了するまで呼び出し元のスレッドをブロックする。
-    keyboard_listener.join()
-    mouse_listener.join()
+    # keyboard_listener.join()
+    # mouse_listener.join()
+
+    # joinを呼び出したスレッドが終了するまで待機。
+    # joinを呼び出したスレッドが終了するまで呼び出し元のスレッドをブロックする。
+    # joinを呼ばない場合、次の処理に移ってしまう = 先にメインスレッドが終了してしまう。
+
+    # joinを呼んでしまうとそこで処理がブロックされてしまうため、後の処理に回すことが出来ない。
+    # が、joinの役割を考えるとjoinを呼び出さずともメインスレッドが終了しなければいいだけの話。
+
+    # 今回の要件は指定時間までスレッドにより入力デバイスを監視すること。
+    # つまり、スレッドを動かした後はWhileで時刻を監視、指定時間を過ぎたら終了・・・のようにすれば良いだけ。
+
+    current = TimeHelper.current()
+    finish = TimeHelper.shift_minutes(current, 3)
+    time_helper = TimeHelper(start=current, shift_step_min=1)
+
+    # finish if current == finish
+    while (
+        TimeHelper.get_hour_and_min(TimeHelper.current()) !=
+        TimeHelper.get_hour_and_min(finish)
+    ):
+        next_shift = time_helper.next_shift()
+        print("next shift", next_shift)
+        while (
+            TimeHelper.get_hour_and_min(TimeHelper.current()) !=
+            TimeHelper.get_hour_and_min(next_shift)
+        ):
+            pass
+        print("finish inner loop at", next_shift)
+    print("finish outer loop")
 
 
 print("start")
 monitoring_input_device()
 print("finish")
-
-"""
-# TODO
-# problem
-    # listenerがスレッドをブロックしているため、他の処理(ex: 時間の判定)に移行できない
-    # コールバック内でフラグ判定するにしても、コールバックは入力がなければ起動しないため入力があってから終了になってしまう
-# solution?
-    # 別スレッドを作成, 別スレッド内で判定してリスナーを止める。
-
-    # 別スレッド内
-        # while start == fin:
-            # do something
-            # ここでスレッドをkill
-            # thread_1.stop
-            # thread_2.stop
-
-    # threading.Event()で行けそう?
-    # あるいは、Synchronous event listening(指定秒数イベントを取得するスタイル) で指定秒数取得する、でも可。(whileで時間を監視, 0.1sや1.0sごとに入力を監視するイメージ)
-"""
