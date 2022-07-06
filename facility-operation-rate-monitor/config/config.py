59bccb23c8654a811cc4213688572d352083c743
NasuPanda <xiyuanjunzhi@gmail.com>
2022-07-06 15:02:53 +0900

    fix: Modified definition of LOG_FILE_PATH

--- facility-operation-rate-monitor/config/config.py
+++ facility-operation-rate-monitor/config/config.py
@@ -1,7 +1,8 @@
 from config import helper
 
 
-LOG_FOLDER = "./logs"
+LOG_FOLDER = "./test/fixtures/duplication_exist"
 FACILITY_NAME = "facility_A"
 log_file_name = helper.log_file_name(FACILITY_NAME)
+log_file_name_resolved_conflict = helper.resolve_filename_conflict(log_file_name, LOG_FOLDER)
 LOG_FILE_PATH = helper.log_file_path(LOG_FOLDER, log_file_name)
