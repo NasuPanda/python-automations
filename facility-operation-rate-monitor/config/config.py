7d9ba1d4bf6f226961c7ce3ac82a864ea3714fe1
NasuPanda <xiyuanjunzhi@gmail.com>
2022-07-07 10:06:25 +0900

    feat: Add determination of whether the log file needs to be recreated

--- facility-operation-rate-monitor/config/config.py
+++ facility-operation-rate-monitor/config/config.py
@@ -7,8 +7,15 @@ config_data = helper.load_yaml(CONFIG_YAML_PATH)
 MONITOR_INTERVAL_MINUTES = config_data["monitor_interval_minutes"]
 LOG_FOLDER = config_data["log_folder"]
 FACILITY_NAME = config_data["facility_name"]
+MONITORED_PROCESS = config_data["monitored_process"]
 CSV_COLUMNS = config_data["csv_columns"]
-# Log file path
+
+# Set log file path
 log_file_name = helper.log_file_name(attached_string=FACILITY_NAME, extension=".csv")
-log_file_name_resolved_conflict = helper.resolve_filename_conflict(log_file_name, LOG_FOLDER)
-LOG_FILE_PATH = helper.log_file_path(LOG_FOLDER, log_file_name_resolved_conflict)
+log_file_path = helper.log_file_fullpath(LOG_FOLDER, log_file_name)
+if helper.need_to_recreate_log_file(log_file_path, list(CSV_COLUMNS.values())):
+    # Resolve log file name if log file needs to be re-created
+    log_file_name_resolved_conflict = helper.resolve_filename_conflict(log_file_name, LOG_FOLDER)
+    LOG_FILE_PATH = helper.log_file_fullpath(LOG_FOLDER, log_file_name_resolved_conflict)
+else:
+    LOG_FILE_PATH = log_file_path
