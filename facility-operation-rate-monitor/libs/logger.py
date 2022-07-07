7c5c6df37a3592c9933febb67f77e517583c0296
NasuPanda <xiyuanjunzhi@gmail.com>
2022-07-07 17:22:18 +0900

    feat: Completed write_log method

--- facility-operation-rate-monitor/libs/logger.py
+++ facility-operation-rate-monitor/libs/logger.py
@@ -8,10 +8,10 @@ from libs import timehelper
 
 @dataclass
 class Log():
-    def __init__(self) -> None:
+    def __init__(self, *flags: bool) -> None:
         self.date_column: str = self.__data_column()
         self.time_column: str = self.__time_column()
-        self.facility_operation_status_column: int = self.__facility_operation_status_column()
+        self.facility_operation_status_column: int = self.__facility_operation_status_column(*flags)
 
     @property
     def data_row_as_dict(self) -> dict[str, str | int]:
@@ -28,8 +28,8 @@ class Log():
         return timehelper.format(timehelper.current(), "short_time")
 
     def __facility_operation_status_column(self, *flags: bool) -> int:
-        # TODO キーマウス等の入力を受け取り判定する
-        # TODO プロセスの稼働状況を受け取り判定する
+        if any(flags):
+            return 1
         return 0
 
 
@@ -38,16 +38,13 @@ class Logger():
         self.log_filepath: str = log_filepath
         self.headers: list[str] = list(config.CSV_COLUMNS.values())
 
-    def write_log(self):
+    def write_log(self, *flags: bool):
         self.__create_log_file_if_needed()
-        log = self.__new_log()
+        log = self.__new_log(*flags)
 
         with open(self.log_filepath, "a", encoding="utf8", newline="") as f:
             writer = csv.DictWriter(f, self.headers)
             writer.writerow(log.data_row_as_dict)
-
-    def __new_log(self):
-        return Log()
 
     def __create_log_file_if_needed(self):
         if self.__exist_log_file():
@@ -58,3 +55,7 @@ class Logger():
 
     def __exist_log_file(self) -> bool:
         return pathlib.Path(self.log_filepath).exists()
+
+    @classmethod
+    def __new_log(cls, *flags: bool):
+        return Log(*flags)
