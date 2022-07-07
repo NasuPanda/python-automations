9be5d3687a8b2f430b0bf8f6b94bdead84f7dba4
NasuPanda <xiyuanjunzhi@gmail.com>
2022-07-07 17:33:12 +0900

    feat: Modified main.py to using Logger and ProcessesMonitor

--- facility-operation-rate-monitor/main.py
+++ facility-operation-rate-monitor/main.py
@@ -1,40 +1,40 @@
+from time import sleep
+from libs.logger import Logger
 from libs.monitors.device import InputDeviceMonitor
-from libs.monitors.system import SystemPerformanceMonitor
+from libs.monitors.process import ProcessesMonitor
 from libs import timehelper
 from libs.timehelper import TimeShifter
 
 
-monitor = InputDeviceMonitor()
-performance_monitor = SystemPerformanceMonitor()
-monitor.start_listener()
+process_monitor = ProcessesMonitor()
 current = timehelper.current()
-finish = timehelper.shift_minutes(current, 3)
 time_shifter = TimeShifter(start=current, shift_step_min=1)
+logger = Logger()
 
 print("開始時刻: ", timehelper.format(current, "long"))
-print("終了予定: ", timehelper.format(finish, "long"))
 
-# Finish if current == finish
-while timehelper.is_faster_than(finish, timehelper.current()):
+while True:
     next_shift = time_shifter.next_shift()
+    device_monitor = InputDeviceMonitor()
+    device_monitor.start_listener()
     print("次の時間まで監視, 入力がなければ非稼働と判定", timehelper.format(next_shift, "long"))
 
     while timehelper.is_faster_than(next_shift, timehelper.current()):
         # パフォーマンスモニターの値を更新
-        performance_monitor.add_current_cpu_usage_to_cpu_usages()
-        performance_monitor.add_current_memory_usage_to_memory_usages()
+        process_monitor.update_process_instance_and_has_process_been_executed()
+        timehelper.sleep()
 
+    print("*" * 50)
     print("1セクションの監視終了: ", timehelper.format(next_shift, "long"))
-    print(f"カウント クリック回数:{monitor.click_count} キー入力回数: {monitor.keystroke_count} マウス移動回数: {monitor.mouse_movement_count}")
+    print(f"クリック回数:{device_monitor.click_count} キー入力回数: {device_monitor.keystroke_count} マウス移動回数: {device_monitor.mouse_movement_count}")
 
-    judge = "入力有" if monitor.existence_of_input else "入力無"
-    print(f"判定: {judge}")
-    print("CPU負荷の平均", performance_monitor.cpu_usage_average)
-    print("メモリ使用量の平均", performance_monitor.memory_usage_average)
+    device_input = "入力有" if device_monitor.existence_of_input else "入力無"
+    print(f"デバイス入力: {device_input}")
+    process_status = "稼働" if process_monitor.has_process_been_executed else "非稼働"
+    print(f"プロセスの稼働: {process_status}")
 
-    monitor.reset_count()
-    performance_monitor.reset()
+    logger.write_log(device_monitor.existence_of_input, process_monitor.has_process_been_executed)
+    print("*" * 50)
 
-monitor.stop_listener()
-print("終了: ", timehelper.format(finish, "long"))
-print("finish")
+    device_monitor.stop_listener()
+    process_monitor.reset_has_process_been_executed()
