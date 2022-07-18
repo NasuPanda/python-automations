b097004dfc1ff17811a174123d757a892da08f88
NasuPanda <xiyuanjunzhi@gmail.com>
2022-07-19 08:51:32 +0900

    docs: Add TODO

---
+++ facility-operation-rate-monitor/to_excel.py
@@ -0,0 +1,19 @@
+# Get user input
+# TODO GUI
+
+
+# Read log
+# TODO libs/log/ 配下に logger.py と reader.py を置くように変更
+
+
+# Split data by date
+# TODO logs/extractor / reader に定義
+# TODO I/O と データの加工 は別枠で扱うべきな気がするので、分けた方が良い気がする
+
+
+# Flag to actual facility operation rate
+# TODO こいつの役割 => データの加工、整形。であれば、Formatterがしっくり来るか？
+
+
+# Write facility operation rate and date to excel
+# TODO excel accessor を持ってくる
