47c2ee59394eb0d2029768e8d0f7c7bcf616ff8e
NasuPanda <xiyuanjunzhi@gmail.com>
2022-08-11 06:48:45 +0900

    feat: Add a driver class to wrapp Selenium Web Driver

---
+++ scraping/libs/driver.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from libs.parser import Parser
+
+
+class WebDriver:
+    def __init__(self) -> None:
+        options = Options()
+        options.add_argument("--headless")
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    def parse_ongoing_titles_in_tonarinoyj(self, url: str) -> dict[str, str]:
+        self.get(url)
+        parser = Parser(self.current_page_source)
+        return parser.parse_ongoing_titles()
+
+    def get(self, url: str) -> None:
+        self.driver.get(url)
+
+    def quit(self) -> None:
+        self.driver.quit()
+
+    @property
+    def current_url(self) -> str:
+        return self.driver.current_url
+
+    @property
+    def current_page_source(self) -> str:
+        return self.driver.page_source
+
+    @property
+    def current_title(self) -> str:
+        return self.driver.title
