c57fabf0641232c3c6f1fdfd401c59c1d8bc409b
NasuPanda <xiyuanjunzhi@gmail.com>
2022-10-12 21:28:27 +0900

    refactor: Put the legend specific position

--- graph-viewer/src/data/graph/graph.py
+++ graph-viewer/src/data/graph/graph.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
+FIGURE_BG_COLOR = "azure"
+SUBPLOT_POSITION = {"left": 0.05, "right": 0.6, "bottom": 0.1, "top": 0.95}
+
+
 def draw_figure_to_canvas(canvas_component, figure) -> FigureCanvasTkAgg:
     """PySimpleGUI の cavas と matplotlib の figure を受け取り、描画する"""
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas_component)
@@ -13,10 +17,11 @@ def draw_figure_to_canvas(canvas_component, figure) -> FigureCanvasTkAgg:
 
 
 class Graph:
-    def __init__(self, canvas_component: tkinter.Canvas, figure_size: tuple[int, int] = (7, 5)) -> None:
-        embed_figure = plt.figure(figsize=figure_size)
-        self.axes = embed_figure.add_subplot(111)
-        self.figure_canvas = draw_figure_to_canvas(canvas_component, embed_figure)
+    def __init__(self, canvas_component: tkinter.Canvas, figure_size: tuple[int, int] = (12, 10)) -> None:
+        self.fig, self.axes = plt.subplots(figsize=figure_size)
+        self.fig.set_facecolor(FIGURE_BG_COLOR)
+        self.fig.subplots_adjust(**SUBPLOT_POSITION)
+        self.figure_canvas = draw_figure_to_canvas(canvas_component, self.fig)
 
     def clear(self) -> None:
         """グラフをクリアする。"""
@@ -32,7 +37,10 @@ class Graph:
         # FIXME x, y ともに受け取る仕様にする？
         # time[s] を強制的に x軸で固定する、とか。
         self.axes.plot(data, label=label)
-        self.axes.legend()
+        self.fig.subplots_adjust(**SUBPLOT_POSITION)
+
+        # self.axes.legend(loc="upper left")
+        self.axes.legend(bbox_to_anchor=(1.00, 1), borderaxespad=0)
 
     def commit_change(self) -> None:
         """グラフにプロットした結果を反映させる。"""
