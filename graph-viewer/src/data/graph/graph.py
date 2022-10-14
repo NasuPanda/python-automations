3c8ad1cbab1ffc89ef9fd8ce5722c08bd8eace22
NasuPanda <xiyuanjunzhi@gmail.com>
2022-10-14 16:17:24 +0900

    fix: Add x and y plot margin globally

--- graph-viewer/src/data/graph/graph.py
+++ graph-viewer/src/data/graph/graph.py
@@ -5,7 +5,9 @@ import tkinter
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-PLOT_FONT = "MS Gothic"
+PLOT_PARAM_FONT = "MS Gothic"
+PLOT_PARAM_X_MARGIN = 0
+PLOT_PARAM_Y_MARGIN = 0
 FIGURE_BG_COLOR = "azure"
 BASELINE_STYLE = "dashed"
 SUBPLOT_POSITION = {"left": 0.05, "right": 0.6, "bottom": 0.1, "top": 0.95}
@@ -23,24 +25,24 @@ class Graph:
     """
     References
         - https://www.haya-programming.com/entry/2018/10/11/030103
-        - https://stackoverflow.com/questions/4700614/how-to-put-the-legend-outside-the-plot
+        - 凡例の位置 : https://stackoverflow.com/questions/4700614/how-to-put-the-legend-outside-the-plot
+        - プロットの余白をグローバルに設定 : https://stackoverflow.com/questions/42668433/matplotlib-globally-set-margins
     """
 
     def __init__(self, canvas_component: tkinter.Canvas, figure_size: tuple[int, int] = (10, 8)) -> None:
-        plt.rcParams["font.family"] = PLOT_FONT
+        plt.rcParams["font.family"] = PLOT_PARAM_FONT
+        plt.rcParams["axes.xmargin"] = PLOT_PARAM_X_MARGIN
+        plt.rcParams["axes.ymargin"] = PLOT_PARAM_Y_MARGIN
 
         self.fig, self.axes = plt.subplots(figsize=figure_size)
         self.fig.set_facecolor(FIGURE_BG_COLOR)
         self.fig.subplots_adjust(**SUBPLOT_POSITION)
         self.figure_canvas = draw_figure_to_canvas(canvas_component, self.fig)
-        # 水平線をもつかどうか True の場合、x_limの値に補正を掛ける
-        self.has_hline = False
 
     def clear(self) -> None:
         """グラフをクリアする。"""
         self.axes.cla()
         self.figure_canvas.draw()
-        self.has_hline = False
 
     def plot(self, y_values: list, label: str, x_values: list | None = None) -> None:
         """グラフにデータをプロットする。
@@ -77,12 +79,8 @@ class Graph:
         color: str = "blue",
         linestyle: str = BASELINE_STYLE,
     ) -> None:
-        _, x_max = self.axes.get_xlim()
-        # NOTE get_xlim の返り値は axes の表示範囲の下限/上限。プロットされた値ではない。
-        # x_lim の値をそのまま参照して h_line を引くと、表示範囲が拡大され続けてしまう。それを防ぐために補正を掛ける。
-        if self.has_hline:
-            x_max = x_max / 1.05
-        self.axes.hlines([h_value], 0, x_max, color, linestyles=linestyle)
+        x_min, x_max = self.axes.get_xlim()
+        self.axes.hlines([h_value], x_min, x_max, color, linestyles=linestyle)
         self.has_hline = True
 
     def commit_change(self) -> None:
