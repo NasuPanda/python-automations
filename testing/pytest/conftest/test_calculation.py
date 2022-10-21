7075598f0baef95fad9877b36d41e716372f9f02
NasuPanda <xiyuanjunzhi@gmail.com>
2022-10-21 15:56:25 +0900

    feat: Add fixtures

---
+++ testing/pytest/conftest/test_calculation.py
@@ -0,0 +1,28 @@
+"""https://docs.pytest.org/en/7.1.x/
+"""
+import os
+
+import pytest
+
+import calculation
+
+
+class TestCal(object):
+    @classmethod
+    def setup_class(cls):
+        cls.cal = calculation.Cal()
+        cls.test_filename = "test.txt"
+
+    def test_add_num_and_double(self, csv_file):
+        print(csv_file)
+        assert self.cal.add_num_and_double(1, 1) == 4
+
+    def test_add_num_and_double_raise_if_receive_strings(self):
+        with pytest.raises(ValueError):
+            self.cal.add_num_and_double("1", "1")
+
+    def test_save(self, tmpdir):
+        self.cal.save(tmpdir, self.test_filename)
+        created_filepath = os.path.join(tmpdir, self.test_filename)
+
+        assert os.path.exists(created_filepath) is True
