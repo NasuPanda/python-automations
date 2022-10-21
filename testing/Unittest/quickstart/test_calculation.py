d982643a059839c736b2f3c214f2de8183027261
NasuPanda <xiyuanjunzhi@gmail.com>
2022-10-21 10:09:16 +0900

    feat: Add exception test

--- testing/Unittest/quickstart/test_calculation.py
+++ testing/Unittest/quickstart/test_calculation.py
@@ -1,17 +1,27 @@
 """https://docs.python.org/ja/3/library/unittest.html
+全てのアサーション : https://docs.python.org/ja/3/library/unittest.html#unittest.TestCase
 """
 import unittest
 
 import calculation
 
 
-# unittest.TestCase を継承する
 class CalTest(unittest.TestCase):
-    # test_method_name とする
+    """Testing Cal.
+
+    - unittest.TestCase を継承する
+    - メソッド名は test_method_name とする
+    """
+
     def test_add_num_and_double(self):
         cal = calculation.Cal()
-        # その他のアサーション : https://docs.python.org/ja/3/library/unittest.html#unittest.TestCase
         self.assertEqual(cal.add_num_and_double(1, 1), 4)
+
+    def test_add_num_and_double_raise(self):
+        cal = calculation.Cal()
+        # 例外処理にはwithステートメントを使う
+        with self.assertRaises(ValueError):
+            cal.add_num_and_double("1", "1")
 
 
 # unittest.main()で実行
